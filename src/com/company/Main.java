abba1a29af61393762417a1dac9667e1c853804c
lesson1 <bayaliev07@gmail.com>
2022-08-17 19:03:19 +0300

    cc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,26 @@
 package com.company;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<City> map =  new TreeSet<>(City.cityComparator);
-City city = new City(495 ,"Москва");
-City city2 = new City(86,"Пекин");
-City city3 = new City(39,"Рим");
-City city4 = new City(34,"Мадрид");
-City city5 = new City(9714,"Дубай");
-map.add(city);
-map.add(city2);
-map.add(city3);
-map.add(city4);
-map.add(city5);
-        for (City set:map) {
-            if (set.getCode() %2==0){
-                System.out.println(set);
+        Set<City> map = new TreeSet<>(City.cityComparator);
+        Set<City> list = new HashSet<>();
+        City city = new City(495, "Москва");
+        City city2 = new City(86, "Пекин");
+        City city3 = new City(39, "Рим");
+        City city4 = new City(34, "Мадрид");
+        City city5 = new City(9714, "Дубай");
+        list.add(city);
+        list.add(city2);
+        list.add(city3);
+        list.add(city4);
+        list.add(city5);
+        for (City i : list) {
+            if (i.getCode() % 2 != 1) {
+                map.add(i);
             }
         }
+        System.out.println(map);
     }
 }
